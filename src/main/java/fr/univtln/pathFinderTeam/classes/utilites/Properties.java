dbabaffcac2ab5c595f6bc94e3f2e3397e55a727
clement farge <farge.clement@gmail.com>
2018-01-22 18:49:25 +0100

    ajout de requetes nommées dans chaque classes

---
+++ src/main/java/fr/univtln/pathFinderTeam/classes/utilites/Properties.java
@@ -0,0 +1,8 @@
+package fr.univtln.pathFinderTeam.utilites;
+
+public class Properties {
+
+    // nom des requetes nommées
+    public static final String FIND_CHARACTER_BY_NAME = "findRPCharacterByName";
+    public static final String FIND_LEVELCLASS_BY_NAME = "findLevelClassByName";
+}
